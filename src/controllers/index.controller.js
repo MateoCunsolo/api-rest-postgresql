00cffcb3c26b611dd05086bea5655f4e377c84ea
Mateo Cunsolo <cunsolo73@gmail.com>
2024-04-11 14:11:49 -0300

    successful connection with flick_finder_db in postgresql

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -15,9 +15,8 @@ const pool = new Pool({
 
 
 const getUsers = async (req, res) => {
-    // const response = await pool.query('SELECT * FROM "user"')
-    // res.status(200).json(response.rows);
-    res.json('users');
+    const response = await pool.query('SELECT * FROM "user"')
+    res.status(200).json(response.rows);
 }
 
 const getUserById = async (req, res) => {
